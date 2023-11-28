39f10f1fac328115204a249bd0fc0023eac59d8f
dpriyanka9692 <152000017+dpriyanka9692@users.noreply.github.com>
2023-11-28 10:18:33 +0000

    introduction file saved

---
+++ 01-besic/priyanka.js
@@ -0,0 +1,10 @@
+console.log("Hello world")
+/*
+javascript is a high level scripting language to use on developing web and mobile application efficienty.
+We use javascript on clint side as well as server side.
+In javascript we do logical implimentation in web pages.
+In today era we use ESB version of javascript,as it is latest one.
+* Esb-Echma script 6(vanilla javascrip)
+
+
+*/
